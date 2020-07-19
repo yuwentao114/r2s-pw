f71b3c8843901183ef6139c25558d0deb6ebaca1
yuwentao114 <49578217+yuwentao114@users.noreply.github.com>
2020-07-19 21:11:32 +0800

    Update remove_unused_config.sh

--- remove_unused_config.sh
+++ remove_unused_config.sh
@@ -13,3 +13,4 @@ find device/ -name distfeeds.conf -delete
 [ -f configs/config_rk3328 ] && echo -e '\nCONFIG_KERNEL_BUILD_USER="Dayong Chen"\nCONFIG_GRUB_TITLE="OpenWrt on Nanopi devices compiled by DayongChen"' >> configs/config_rk3328
 [ -f configs/config_h5 ]     && echo -e '\nCONFIG_KERNEL_BUILD_USER="Dayong Chen"\nCONFIG_GRUB_TITLE="OpenWrt on Nanopi devices compiled by DayongChen"' >> configs/config_h5
 [ -f configs/config_h3 ]     && echo -e '\nCONFIG_KERNEL_BUILD_USER="Dayong Chen"\nCONFIG_GRUB_TITLE="OpenWrt on Nanopi devices compiled by DayongChen"' >> configs/config_h3
+
